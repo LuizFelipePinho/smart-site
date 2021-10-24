078d569b278a8aa8db77507cd0cc0f5bc691b6c3
LuizFelipePinho <luizfelipesilva458@gmail.com>
2021-10-24 20:52:42 -0300

    header feito

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Logo from "../Logo/Logo";
+import './Header.css'
+
+const Header = () => {
+
+    return (
+
+        <header className="header">
+
+            <Logo></Logo>
+
+        </header>
+    )
+}
+
+export default Header
