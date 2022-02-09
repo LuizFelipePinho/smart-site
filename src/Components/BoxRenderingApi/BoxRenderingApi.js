4d02b0504b148a342772d37c4c028309c0efc3a5
LuizFelipePinho <luizfelipesilva458@gmail.com>
2022-02-09 18:47:29 -0300

    add msg not found units

--- src/Components/BoxRenderingApi/BoxRenderingApi.js
+++ src/Components/BoxRenderingApi/BoxRenderingApi.js
@@ -3,11 +3,21 @@ import BoxUni from './BoxUni/BoxUni'
 
 const BoxRenderingApi = ({data}) => {
 
+    let units = data.map( (unidade) => {
+        return( <BoxUni key={unidade.id} data={unidade} />)
+    })
+
+    let unitNotFound = <div className='containerUnitNotFound'>
+            <p className='msgUnitNotFound'>Nenhuma unidade encontrada</p>
+        </div>
+
     return(
         <div className="containerBoxUniRender" id="containerBoxUniRender">
-            { data.map( (unidade) => {
-                return( <BoxUni key={unidade.id} data={unidade} />)
-            })}
+
+            {
+                data.length > 1 ? units : unitNotFound
+            }
+
         </div>
         )
 } 
